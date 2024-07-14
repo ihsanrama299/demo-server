c9ae044524136aa702fa4a65b4bccd851c8bb950
ihsanrama299 <ihsanramadhan299@gmail.com>
2024-07-14 17:35:27 +0800

    fixed cors 2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { default: mongoose } = require("mongoose");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
